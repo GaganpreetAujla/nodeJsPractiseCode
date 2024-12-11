5dddec78fa7b7273d04486f2d2a31dfc95362d70
Gaganpreet Aujla <gk1869932@gmail.com>
2024-12-10 23:37:05 -0500

    update all file in nodeJsPractise file

---
+++ app.js
@@ -0,0 +1,31 @@
+//         eXAMPLE FOR USING JS IN GITBASH TERMINAL
+/*let n=4;
+
+//for(let i = 0; i<n;i++){
+  //  console.log("hello ,", i);
+}
+  */
+
+
+//      PROCESS.ARGV
+/*let args = process.argv;
+
+for( let i=2;i<args.length;i++){
+    console.log("hello ", args[i]);
+}
+    */
+
+
+
+
+//          module require example (also some code of this in math.js)
+/* 
+let math = require("./math");
+console.log(math.pi);
+*/
+
+
+
+const info = require("./fruits");
+console.log(info);
+
---
+++ math.js
@@ -0,0 +1,16 @@
+
+
+//      MODULE REQUIRE EXAMPLE
+/*const pi = 3.14;
+const sum = (a,b) => a + b;
+const mul = (a,b) => a * b;
+
+
+let obj = {
+    pi: pi,
+    sum : sum,
+    mul : mul
+
+}
+module.exports = obj;
+*/
